98c311ebf706bf8a0ac138fd5377b3d629180097
Dinu V <vdinuu@gmail.com>
2024-08-01 10:30:56 -0400

    Implementing logs

---
+++ src/main/java/utils/Logs.java
@@ -0,0 +1,37 @@
+package utils;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Logs {
+    private static Logger log = LoggerFactory.getLogger(Logs.class);
+    public static void startTestCase(String testCaseName) {
+        log.info("****************************************************************************************");
+        log.info("****************************************************************************************");
+        log.info("$$$$$$$$$$$$$$$$$$$$$                 " + testCaseName + "       $$$$$$$$$$$$$$$$$$$$$$$$$");
+        log.info("****************************************************************************************");
+        log.info("****************************************************************************************");
+    }
+
+    public static void endTestCase() {
+        log.info("XXXXXXXXXXXXXXXXXXXXXXX             " + "-E---N---D-" + "             XXXXXXXXXXXXXXXXXXXXXX");
+        log.info("X");
+        log.info("X");
+    }
+    public static void info(String message) {
+        log.info(message);
+    }
+
+    public static void warn(String message) {
+        log.warn(message);
+    }
+
+    public static void error(String message) {
+        log.error(message);
+    }
+
+    public static void debug(String message) {
+        log.debug(message);
+    }
+}
